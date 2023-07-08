1a534468bcf2c22a063e6caabbb3ef47db320e10
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-08 11:23:31 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+
+const Comp = (props) => {
+  const [text, setText] = useState('');
+
+  return (
+    <div className="parent">
+      <h1>{props.parent}</h1>
+      <h4>{text}</h4>
+      <div className="child">
+        <h2>{props.child}</h2>
+        <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
+      </div>
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <h1>MY REACT APP</h1>
+    <div id="main">
+        <Comp parent="Parent Component" child="Child Component" />
     </div>
   )
 }
